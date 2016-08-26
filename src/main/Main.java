f08b4ecdaf4f20b15e7957386993420081901fcd
Kevin Schmitt <kevin.schmitt@mni.thm.de>
2016-08-26 16:05:17 +0200

    Initial Commit

---
+++ src/main/Main.java
@@ -0,0 +1,27 @@
+package main;
+
+/**
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
